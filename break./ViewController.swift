98ad3935a0739546833a43b687982fa84c0177ff
Andrew Clissold <andrewclissold@gmail.com>
2014-06-16 23:16:04 -0400

    Create main view layout

--- break./ViewController.swift
+++ break./ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func preferredStatusBarStyle() -> UIStatusBarStyle  {
+        return .LightContent
+    }
 
 }
 
