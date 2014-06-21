f7f861fa768fbe283487f2911ba8d4b2215e7ccb
Andrew Clissold <andrewclissold@gmail.com>
2014-06-21 19:19:56 -0400

    Add Settings view
    Created the Settings view controller, made an initial view mockup in the
    storyboard, and renamed ViewController -> BreakViewController.

--- break./BreakViewController.swift
+++ break./BreakViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BreakViewController.swift
 //  break.
 //
 //  Created by Andrew Clissold on 6/15/14.
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class BreakViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ break./SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  break.
+//
+//  Created by Andrew Clissold on 6/16/14.
+//  Copyright (c) 2014 Andrew Clissold. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    @IBAction func back() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
