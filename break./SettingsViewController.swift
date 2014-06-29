7e8e2669c106b6944316ec5db200db423135b112
Andrew Clissold <andrewclissold@gmail.com>
2014-06-29 11:00:44 -0400

    Set the global tint color

--- break./SettingsViewController.swift
+++ break./SettingsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
 
+    @IBOutlet var doNotDisturbSwitch: UISwitch
+
     @IBAction func back() {
         dismissViewControllerAnimated(true, completion: nil)
     }
