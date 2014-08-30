d3758054d0eb2f1648c28eca945a07193a297e43
Andrew Clissold <andrewclissold@gmail.com>
2014-08-29 21:46:10 -0400

    Make cells selectable

--- break./RepeatViewController.swift
+++ break./RepeatViewController.swift
@@ -10,4 +10,22 @@ import UIKit
 
 class RepeatViewController: UITableViewController {
 
+    var previousCell: UITableViewCell?
+
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        selectCellAtIndexPath(indexPath)
+    }
+
+    func selectCellAtIndexPath(indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        cell.accessoryType = .Checkmark
+
+        if cell !== previousCell {
+            previousCell?.accessoryType = .None
+            previousCell = cell
+        }
+    }
+
 }
