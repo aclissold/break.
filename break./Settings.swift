2a6a82f0be23c6a6985e3955a6e929fb172605f6
Andrew Clissold <andrewclissold@gmail.com>
2014-08-30 19:20:40 -0400

    Enable Settings.frequency and Settings.repeat

--- break./Settings.swift
+++ break./Settings.swift
@@ -23,21 +23,39 @@ struct Settings {
     static var from:      NSDate         = NSDate()             {didSet{synchronize()}}
     static var to:        NSDate         = NSDate()             {didSet{synchronize()}}
     static var repeat:    NSCalendarUnit = .CalendarUnitWeekday {didSet{synchronize()}}
-
 }
 
+private let queue = dispatch_queue_create(nil, nil)
+
 private func synchronize() {
-    let notification = UILocalNotification()
-    if Settings.frequency != .Every60Minutes { p("TODO"); return }
-
     UIApplication.sharedApplication().cancelAllLocalNotifications()
+    dispatch_async(queue) {
+        scheduleNotifications(frequency: Settings.frequency.toRaw())
+    }
+}
 
-    notification.fireDate = NSDate(timeIntervalSinceNow: 5)
-    notification.timeZone = NSTimeZone.defaultTimeZone()
-    notification.category = "Snooze"
-    notification.repeatInterval = .CalendarUnitHour
-    notification.alertBody = "It's time for your typing break!"
-    notification.soundName = UILocalNotificationDefaultSoundName
+// Since these recur indefinitely, it's fine if they're scheduled in the past.
+private func scheduleNotifications(#frequency: Int) {
+    let calendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)
+    var components = calendar.componentsInTimeZone(NSTimeZone.defaultTimeZone(), fromDate: NSDate())
 
-    UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    var minutes = 0
+    while minutes < 24*60 {
+        components.second = 0
+        components.minute = minutes % 60
+        components.hour   = minutes / 60
+        let fireDate = calendar.dateFromComponents(components)
+
+        let notification = UILocalNotification()
+        notification.fireDate = fireDate
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        notification.category = "Snooze"
+        notification.repeatInterval = Settings.repeat
+        notification.alertBody = "It's time for your typing break!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+
+        minutes += frequency
+    }
 }
