d11177fd382a36c38dedcf3b7b0ac51d793ac04a
Andrew Clissold <andrewclissold@gmail.com>
2014-09-16 02:34:04 -0400

    Animate hiding and showing the banner view

--- break./BannerContainerViewController.swift
+++ break./BannerContainerViewController.swift
@@ -13,16 +13,17 @@ class BannerContainerViewController: UIViewController, ADBannerViewDelegate {
 
     var bannerView: ADBannerView!
     var constraint: NSLayoutConstraint!
+    var bannerIsVisible = false
 
     override func viewDidLoad() {
         bannerView = ADBannerView(frame: CGRect(x: 0, y: 0, width: 320, height: 66))
+        bannerView.delegate = self
         view.addSubview(bannerView)
 
         bannerView.setTranslatesAutoresizingMaskIntoConstraints(false)
         let height = bannerView.frame.size.height
         addConstraint("H:|-0-[bannerView]-0-|")
         addConstraint("V:[bannerView]-(\(-height))-|")
-
     }
 
     func addConstraint(format: String) {
@@ -44,20 +45,39 @@ class BannerContainerViewController: UIViewController, ADBannerViewDelegate {
     }
 
     func updateInsets() {
-        let insets = UIEdgeInsets(top: settingsTableView.contentInset.top, left: 0, bottom: bannerView.frame.size.height, right: 0)
-        let contentSize = CGSize(width: settingsTableView.frame.size.width, height: settingsTableView.frame.size.height + bannerView.frame.size.height)
-        settingsTableView.contentSize = contentSize
-        settingsTableView.contentInset = insets
-        settingsTableView.scrollIndicatorInsets = insets
-        constraint.constant = -bannerView.frame.size.height
+        var insets: UIEdgeInsets!
+        var contentSize: CGSize!
+        var constant: CGFloat!
+        if bannerIsVisible {
+            insets = UIEdgeInsets(top: settingsTableView.contentInset.top, left: 0, bottom: bannerView.frame.size.height, right: 0)
+            contentSize = CGSize(width: settingsTableView.frame.size.width, height: settingsTableView.frame.size.height + bannerView.frame.size.height)
+            constant = 0
+        } else {
+            insets = UIEdgeInsets(top: settingsTableView.contentInset.top, left: 0, bottom: 0, right: 0)
+            contentSize = CGSize(width: settingsTableView.frame.size.width, height: settingsTableView.frame.size.height + bannerView.frame.size.height)
+            constant = -bannerView.frame.size.height
+        }
+
+        view.layoutIfNeeded()
+        UIView.animateWithDuration(0.4) {
+            self.constraint.constant = constant
+            self.view.layoutIfNeeded()
+            settingsTableView.contentSize = contentSize
+            settingsTableView.contentInset = insets
+            settingsTableView.scrollIndicatorInsets = insets
+        }
     }
 
     // MARK: ADBannerViewDelegate
 
     func bannerViewDidLoadAd(banner: ADBannerView!) {
+        bannerIsVisible = true
+        updateInsets()
     }
 
     func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        bannerIsVisible = false
+        updateInsets()
     }
 
 }
