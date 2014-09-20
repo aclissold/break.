a43f65c4e99f695aabc6510d5872703c752d7ca2
Andrew Clissold <andrewclissold@gmail.com>
2014-09-20 13:50:42 -0400

    Move a single-view architecture

--- break./BreakViewController.swift
+++ break./BreakViewController.swift
@@ -1,28 +1,52 @@
 //
-//  BreakViewController.swift
+//  SettingsViewController.swift
 //  break.
 //
-//  Created by Andrew Clissold on 6/15/14.
+//  Created by Andrew Clissold on 6/16/14.
 //  Copyright (c) 2014 Andrew Clissold. All rights reserved.
 //
 
 import UIKit
 
-class BreakViewController: UIViewController {
+weak var settingsTableView: UITableView! // for BannerContainerViewController
+
+class BreakViewController: UITableViewController {
+
+    @IBOutlet weak var silenceSwitch: UISwitch!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var frequencyLabel: UILabel!
+    @IBOutlet weak var repeatLabel: UILabel!
+
+    let frequencies = [20: "20", 30: "30", 60: "60", 90: "90"]
+    let repeats: [UInt: String] = [
+        NSCalendarUnit.DayCalendarUnit.toRaw(): "Daily",
+        NSCalendarUnit.WeekdayCalendarUnit.toRaw(): "Weekdays"]
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        settingsTableView = tableView
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let frequency = defaults.integerForKey("frequency")
+        let repeat = defaults.integerForKey("repeat")
+        let silence = defaults.boolForKey("silence")
+
+        silenceSwitch.on = silence
+        frequencyLabel.text = "Every \(frequency) minutes"
+        repeatLabel.text = repeats[UInt(repeat)]
     }
 
-    override func preferredStatusBarStyle() -> UIStatusBarStyle  {
-        return .LightContent
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
+    @IBAction func silenceSwitchToggled(sender: UISwitch) {
+        Settings.silence = sender.on
+    }
+
+    @IBAction func back() {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
-
