8251d750e8e26fbb64f748b7b247c455a266a9b6
Andrew Clissold <andrewclissold@gmail.com>
2014-09-20 14:11:34 -0400

    Prepare navigation bar for parallax logo

--- break./AppDelegate.swift
+++ break./AppDelegate.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+let globalTintColor = UIColor(red: 220.0/255, green: 47.0/255, blue: 43.0/255, alpha: 1)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        // Theme.
+        UINavigationBar.appearance().barTintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.clearColor()]
+
+        // Configure notifications.
         let snoozeAction = UIMutableUserNotificationAction()
         snoozeAction.identifier = "Snooze"
         snoozeAction.title = "Snooze"
