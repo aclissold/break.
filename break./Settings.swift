8b4ec82ea724bcf09a5df10772ecf8da75c2381e
Andrew Clissold <andrewclissold@gmail.com>
2014-10-13 13:03:43 -0400

    Remove FromTo view

--- break./Settings.swift
+++ break./Settings.swift
@@ -19,8 +19,6 @@ enum Frequency: Int {
 struct Settings {
     static var silence:   Bool           = false                {didSet{synchronize()}}
     static var frequency: Frequency      = .Every60Minutes      {didSet{synchronize()}}
-    static var from:      NSDate         = NSDate()             {didSet{synchronize()}}
-    static var to:        NSDate         = NSDate()             {didSet{synchronize()}}
     static var repeat:    NSCalendarUnit = .CalendarUnitWeekday {didSet{synchronize()}}
 }
 
