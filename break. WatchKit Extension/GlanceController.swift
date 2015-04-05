687122d131831b6005a4a0e06e4b88e82da4b1b7
Andrew Clissold <andrewclissold@gmail.com>
2015-04-04 22:41:26 -0400

    Implement Glance countdown timer

--- break. WatchKit Extension/GlanceController.swift
+++ break. WatchKit Extension/GlanceController.swift
@@ -9,21 +9,67 @@
 import WatchKit
 import Foundation
 
-
 class GlanceController: WKInterfaceController {
 
     @IBOutlet weak var timer: WKInterfaceTimer!
+
+    var calendar = NSCalendar.currentCalendar()
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
     }
 
+    func soonest(dates: [NSDate]) -> NSDate {
+        var soonestDate = dates[0]
+        for date in dates {
+            if date.compare(soonestDate) == .OrderedAscending {
+                soonestDate = date
+            }
+        }
+        return soonestDate
+    }
+
     override func willActivate() {
         super.willActivate()
+        setUpTimer()
+    }
 
-        let calendar = NSCalendar.currentCalendar()
-        let today = NSDate()
-        let date = calendar.dateByAddingUnit(.SecondCalendarUnit, value: 60, toDate: today, options: NSCalendarOptions.SearchBackwards)!
+    override func didDeactivate() {
+        timer.stop()
+        super.didDeactivate()
+    }
+
+    func setUpTimer() {
+        let now = NSDate()
+
+        let frequency = userDefaults.integerForKey("frequency")
+        var date: NSDate!
+        switch (frequency) {
+        case 20:
+            let date1 = calendar.nextDateAfterDate(now, matchingUnit: .MinuteCalendarUnit, value: 0, options: .MatchStrictly)!
+            let date2 = calendar.nextDateAfterDate(now, matchingUnit: .MinuteCalendarUnit, value: 20, options: .MatchStrictly)!
+            let date3 = calendar.nextDateAfterDate(now, matchingUnit: .MinuteCalendarUnit, value: 40, options: .MatchStrictly)!
+            date = soonest([date1, date2, date3])
+        case 30:
+            let date1 = calendar.nextDateAfterDate(now, matchingUnit: .MinuteCalendarUnit, value: 0, options: .MatchStrictly)!
+            let date2 = calendar.nextDateAfterDate(now, matchingUnit: .MinuteCalendarUnit, value: 30, options: .MatchStrictly)!
+            date = soonest([date1, date2])
+        case 60:
+            date = calendar.nextDateAfterDate(now, matchingUnit: .MinuteCalendarUnit, value: 0, options: .MatchStrictly)!
+        case 90:
+            var dates = [NSDate]()
+            var minutes = 0
+            for hour in [0, 1, 3, 4, 6, 7, 9, 10] {
+                let components = NSDateComponents()
+                components.hour = hour
+                components.minute = 30 * (hour % 3) // 0, 30, 0, ...
+                let date = calendar.nextDateAfterDate(now, matchingComponents: components, options: .MatchStrictly)!
+                dates.append(date)
+            }
+            date = soonest(dates)
+        default:
+            date = nil
+        }
         timer.setDate(date)
         timer.start()
     }
